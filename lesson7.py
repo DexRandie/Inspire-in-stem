af033cf70cc424bfdb853d29edef7f9dc3015290
DexRandie <alajirodneyson@gmail>
2022-05-19 11:17:02 +0300

    i learnt about loops

---
+++ lesson7.py
@@ -0,0 +1,25 @@
+#learning about lists
+plate_number = ['QRS434', 'QWE5353','QREW434']
+motorcycle = ['hoods','njamps',' honda']
+#print(motorcycle)
+#print(motorcycle[0])
+motorcycle_owner = "flora"
+#print(motorcycle[1])
+#print("i love" + str(motorcycle[ 2]))
+#removing an item from a list
+#print("i want a" + str(motorcycle[2]) + " which is " + str(plate_number[0]))
+#del motorcycle[0]
+#print(motorcycle)
+#del motorcycle[1] 
+#del plate_number[0]
+#del plate_number[1]
+#print("i want " + str(motorcycle) + " which is " + str(plate_numbe
+motorcycle.remove('njamps')
+print(motorcycle)
+
+print("iam " ,(motorcycle_owner) , " owning a ", (motorcycle[0]) , " which is " , (plate_number[0]))
+
+
+
+
+
---
+++ lesson8.py
@@ -0,0 +1,13 @@
+#learning about the lists
+#plate_number = ['1234' , '4567' ,'8907']
+#$motorcycle = ['honda' , 'boxer' , 'tiger'] 
+#print(motorcycle)
+school = ['joy' , 'hope' , 'mercy' , 'happy']
+pupil=['pop' ,'lol','kok','joj']
+#print("i am ",(pupil[0]) ,"and attend ",(school[0]))
+#print("i am ",(pupil[1]) ,"and attend ",(school[1]))
+#print("i am ",(pupil[2]) ,"and attend ",(school[2]))
+#print("i am ",(pupil[3]) ,"and attend ",(school[3]))
+
+for pupil in pupil:
+    print('hello i am pupil',(pupil))
